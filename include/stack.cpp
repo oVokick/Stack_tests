690788ca7accb7a4daf554a23b4cb2285ac4b109
oVokick <samsungoganes@gmail.com>
2016-09-18 20:39:49 +0300

    Update stack.cpp

--- include/stack.cpp
+++ include/stack.cpp
@@ -1,11 +1,12 @@
+#ifndef stack_cpp
 #define stack_cpp
 #pragma once
-
 
 #include <iostream>
 #include <cstdlib>
 #include <string>
-using namespace std
+
+using namespace std;
 
 size_t max(size_t a, size_t b) {
     return a > b ? a : b;
