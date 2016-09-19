eb169e0d96408c5cfa7bcf8a984837a783fe9854
oVokick <samsungoganes@gmail.com>
2016-09-19 17:21:53 +0300

    Проверка на самоприсваивание, new_with_copy

--- include/stack.cpp
+++ include/stack.cpp
@@ -9,9 +9,9 @@ size_t max(size_t a, size_t b) {
 }
 
 template <typename T>
-T* new_with_copy(const T *tmp, count, array_size) {
-    array_ = new T[array_size];
-    copy(tmp, tmp + count, array_);\
+T* new_with_copy(const T *tmp, size_t count, size_t array_size) {
+T *array_ = new T[array_size];
+copy(tmp, tmp + count, array_);
     return array_;
 }
 
@@ -100,13 +100,14 @@ Stack<T>::Stack(const Stack &tmp)
         :   count_(tmp.count_),
             array_size_(tmp.array_size_)
 {
-    new_with_copy(tmp.array_);
+    array_ = new_with_copy(tmp.array_, count_, array_size_);
 }
 
 template <typename T>
 Stack<T>& Stack<T>::operator=(const Stack<T> &tmp) {
+    if (this == &tmp) {}
     count_ = tmp.count_;
     array_size_ = tmp.array_size_;
-    new_with_copy(tmp.array_);
+    array_ = new_with_copy(tmp.array_, count_, array_size_);
     return *this;
 }
