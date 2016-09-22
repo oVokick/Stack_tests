81831d18b3dfdbcb8a5bb20c0068661258144404
oVokick <samsungoganes@gmail.com>
2016-09-22 13:48:06 +0300

    Дополнил тесты

--- test/source/init.cpp
+++ test/source/init.cpp
@@ -28,6 +28,14 @@ SCENARIO("prisv", "[prisv]"){
   s.push(1);
   Stack<int> s2;
   s2 = s;
-  REQUIRE(s.count() == 1);
-  REQUIRE(s.pop() == 1);
+  REQUIRE(s.count() == s2.count());
+  REQUIRE(s.pop() == s2.pop());
+}
+
+SCENARIO("copy", "[copy]"){
+    Stack<int> s;
+    s.push(3);
+    Stack<int> s2(s);
+    REQUIRE(s2.count() == 3);
+    REQUIRE(s2.pop() == 3);
 }
