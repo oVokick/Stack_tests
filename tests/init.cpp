6859f35aa85ace0de8c48afc9e4759e08a178eac
simplydesigner <simplydesigner@ya.ru>
2016-11-20 09:34:08 +0300

    added test for ~Allocator

--- tests/init.cpp
+++ tests/init.cpp
@@ -1,7 +1,6 @@
 #include <stack.hpp>
 #include <catch.hpp>
 #include <iostream>
-using namespace std;
 
 SCENARIO("count", "[count]"){
   Stack<int> s;
@@ -59,6 +58,42 @@ SCENARIO("empty3", "[empty3]"){
   s1.push(2);
   s1.pop();
   s1.top();
-  
+
   REQUIRE(s1.empty()==false);
+}
+
+class Test
+{
+public:
+  static ssize_t  counter;
+  Test()
+  {
+    std::cout << __PRETTY_FUNCTION__ << std::endl;
+    ++counter;
+  }
+  Test( Test const & )
+  {
+   std::cout << __PRETTY_FUNCTION__ << std::endl;
+    ++counter;
+  }
+  ~Test()
+  {
+    std::cout << __PRETTY_FUNCTION__ << std::endl;
+    --counter;
+  }
+};
+
+ssize_t Test::counter = 0;
+
+SCENARIO("Allocator::~Allocator") {
+
+  {
+    Allocator<Test> allocator{ 4 };
+    allocator.construct( allocator.get() + 1, Test() );
+    allocator.construct( allocator.get() + 2, Test() );
+
+    REQUIRE( Test::counter == 2 );
+  }
+
+  REQUIRE( Test::counter == 0 );
 }
