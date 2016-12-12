a43251f74172020cde7e3f0109d5b33cf818a0b4
oVokick <samsungoganes@gmail.com>
2016-12-12 17:26:46 +0400

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -1,100 +1,62 @@
-#include <stack.hpp>
+#include "stack.cpp"
 #include <catch.hpp>
 #include <iostream>
+#include <thread>
+#include<mutex>
 using namespace std;
-
+ 
 SCENARIO("count", "[count]"){
-  Stack<int> s;
+  stack<int> s;
   s.push(1);
   REQUIRE(s.count()==1);
 }
 
 SCENARIO("push", "[push]"){
-  Stack<int> s;
+  stack<int> s;
   s.push(1);
   REQUIRE(s.count()==1);
-
 }
 
-SCENARIO("top", "[top]"){
-  Stack<int> s;
+SCENARIO("pop", "[pop]"){
+  stack<int> s;
   s.push(1);
-  s.push(2);
-  s.push(3);
-  s.pop();
-
-  REQUIRE(s.top()==2);
-}
-SCENARIO("operprisv", "[operprisv]"){
-  Stack<int> s1;
-  s1.push(1);
-  Stack<int> s2;
-  s2=s1;
-  REQUIRE(s1.count()==s2.count());
+  REQUIRE(*(s.pop())==1);	
+  REQUIRE(s.count()==0);
 }
 
-SCENARIO("const", "[constr]"){
-  Stack<int> s1;
-  s1.push(1);
-  Stack<int> s2=s1;
-  REQUIRE(s1.count()==s2.count());
+SCENARIO("prisv", "[prisv]"){
+  stack<int> s;
+  s.push(1);
+  stack<int> s2;
+  s2=s;
+  REQUIRE(s.count()==1);
 }
 
 SCENARIO("empty", "[empty]"){
-  Stack<int> s1;
+  stack<int> s1, s2;
   s1.push(1);
-  REQUIRE(s1.empty()==false);
+  REQUIRE(!s1.empty());
+  REQUIRE(s2.empty());
 }
 
-SCENARIO("empty2", "[empty2]"){
-  Stack<int> s1;
-  s1.push(1);
-  s1.pop();
-  REQUIRE(s1.empty()==true);
-}
 
-SCENARIO("empty3", "[empty3]"){
-  Stack<int> s1;
-  s1.push(1);
-  s1.push(2);
-  s1.pop();
-  s1.top();
-  
-  REQUIRE(s1.empty()==false);
-}
-
-class Test
-{
-public:
-  static ssize_t  counter;
-  Test()
-  {
-    std::cout << __PRETTY_FUNCTION__ << std::endl;
-    ++counter;
-  }
-  Test( Test const & )
-  {
-   std::cout << __PRETTY_FUNCTION__ << std::endl;
-    ++counter;
-  }
-  ~Test()
-  {
-    std::cout << __PRETTY_FUNCTION__ << std::endl;
-    --counter;
-  }
-};
-
-ssize_t Test::counter = 0;
-
-SCENARIO("Allocator::~Allocator") {
-
-  {
-    Allocator<Test> allocator{ 4 };
-    allocator.construct( allocator.get() + 1, Test() );
-    allocator.construct( allocator.get() + 2, Test() );
-
-    REQUIRE( Test::counter == 2 );
-  }
-
-  REQUIRE( Test::counter == 0 );
+SCENARIO("threads", "[threads]"){
+  stack<int> s;
+  s.push(1);
+  s.push(2);
+  s.push(3);
+	std::thread t1([&s](){
+		for (int i = 0; i < 5; i++) {
+			s.push(i + 4);
+		}
+	});
+	std::thread t2([&s](){
+		for (int i = 0; i < 5; i++)
+		{
+			s.pop();
+		}
+	});
+	t1.join();
+	t2.join();
+  REQUIRE(s.count()==3);
 }
